a60a49f7dbd5290a0c9af83becce6b01be63a611
Miao <sam1016yu@gmail.com>
2019-01-24 22:25:20 +0800

    Update fcnSpatialTemporal.m
    first draft finished

--- fcnSpatialTemporal.m
+++ fcnSpatialTemporal.m
@@ -1,4 +1,4 @@
-function Fs = fcnSpatialTemporal(ptn0_cand,ptn1_cand,ptn0_raw,ptn1_raw,road_network)
+function F = fcnSpatialTemporal(ptn0_cand,ptn1_cand,ptn0_raw,ptn1_raw,delta_t,edg0_cand,edg1_cand,G,node_table,road_network)
 %fcnSpatial:spatial analysis function
 %   Implementation of equation 3 in the paper
 %   Input:
@@ -6,14 +6,13 @@ function Fs = fcnSpatialTemporal(ptn0_cand,ptn1_cand,ptn0_raw,ptn1_raw,road_netw
 %   ptn0_raw,ptn1_raw: GPS points of which candidate points belong
 %   road_network: output of splitRoad2Cell
 %%
-Ncs = fcnNormal(ptn1_cand,ptn1_raw);
-% distance between two GPS points
-dist_actual = distance(fliplr(ptn0_raw),fliplr(ptn1_raw),almanac('earth', 'wgs84'));
-
-
+[path_edges,dist_min] = findShortestPath(G,node_table,ptn0_cand,edg0_cand,ptn1_cand,edg1_cand,road_network);
+Fs = fcnSpatial(ptn1_cand,ptn0_raw,ptn1_raw,dist_min);
+Ft = fcnTemporal(delta_t,path_edges,dist_min,road_network);
+F = Fs * Ft;
 end
 
-function Fs = fcnSpatial(ptn0_cand,ptn1_cand,ptn0_raw,ptn1_raw,road_network)
+function Fs = fcnSpatial(ptn1_cand,ptn0_raw,ptn1_raw,dist_shortest)
 %fcnSpatial:spatial analysis function
 %   Implementation of equation 3 in the paper
 %   Input:
@@ -24,8 +23,7 @@ function Fs = fcnSpatial(ptn0_cand,ptn1_cand,ptn0_raw,ptn1_raw,road_network)
 Ncs = fcnNormal(ptn1_cand,ptn1_raw);
 % distance between two GPS points
 dist_actual = distance(fliplr(ptn0_raw),fliplr(ptn1_raw),almanac('earth', 'wgs84'));
-
-
+Fs = Ncs*dist_actual/dist_shortest;
 end
 
 function Np = fcnNormal(ptn_cand,ptn_raw)
@@ -37,4 +35,11 @@ function Np = fcnNormal(ptn_cand,ptn_raw)
 mu = 0; sigma = 20/1000;
 dist = distance(fliplr(ptn_cand),fliplr(ptn_raw),almanac('earth', 'wgs84'));
 Np = exp((dist-mu)^2/(2*sigma^2))/(sqrt(2*pi)*sigma);
+end
+
+function Ft = fcnTemporal(delta_t,path_edges,dist_min,road_network)
+    avg_speed = dist_min / delta_t * 60 * 60;
+    avg_speed_vec = repmat(avg_speed,1,length(path_edges));
+    speed_limit_vec = getSpeedLimits(road_network.Type(ismember(road_network.EdgeID,path_edges)));
+    Ft = pdist([avg_speed_vec;speed_limit_vec],'cosine');
 end
