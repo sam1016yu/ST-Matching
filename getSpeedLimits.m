ec13b7eae63d8e2eff2bcbc8ccf7f9b6945360d0
Miao <sam1016yu@gmail.com>
2019-01-24 22:25:06 +0800

    Create getSpeedLimits.m

---
+++ getSpeedLimits.m
@@ -0,0 +1,34 @@
+function [speed_limits] = getSpeedLimits(road_network)
+%Get speed limit of each road type
+    road_types = road_network.Type;
+    speed_limits = zeros(size(road_types)');
+    for type_idx = 1:length(road_types)
+        speed_limits(type_idx) = type2limit(road_type(type_idx));
+    end
+end
+
+function speed_limit = type2limit(rtype)
+%https://wiki.openstreetmap.org/wiki/OSM_tags_for_routing/Maxspeed
+switch rtype
+    case '#'
+        speed_limit = 40;
+    case 'motorway'
+        speed_limit = 120;
+    case 'primary '
+        speed_limit = 100;
+    case 'residential'
+        speed_limit = 40;
+    case 'secondary'
+        speed_limit = 60;
+    case 'service'
+        speed_limit = 60;
+    case 'tertiary'
+        speed_limit = 40;
+    case 'trunk'
+        speed_limit = 60;
+    case 'unclassified '
+        speed_limit = 40;
+    otherwise
+        speed_limit = 40;
+end
+end
